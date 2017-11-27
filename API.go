35e7ff5d28d047a08cd98f27ad512d9b8a9e37c2
jersear8278 <jersear8278@gmail.com>
2017-11-28 06:54:22 +0800

    first

---
+++ API.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    //"fmt"
+    //"html"
+    "log"
+    "net/http"
+    "encoding/json"
+    "io/ioutil"
+)
+
+type Todo struct {
+    Name      string
+    Completed bool
+}
+
+type Todos []Todo
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+      b,_ := ioutil.ReadFile("output.json");
+      rawIn := json.RawMessage(string(b))
+      var objmap map[string]*json.RawMessage
+      err := json.Unmarshal(rawIn, &objmap)
+      if err != nil {
+      }
+
+      json.NewEncoder(w).Encode(objmap)
+//  json.NewEncoder(w).Encode(b)
+    })
+
+    log.Fatal(http.ListenAndServe(":80", nil))
+
+}
---
+++ Api_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "testing"
+)
+
+func Test_API(t *testing.T) {
+
+}
